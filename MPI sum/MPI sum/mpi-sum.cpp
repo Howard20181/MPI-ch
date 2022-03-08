7550442470c7fb802424bb3b4c5cdba113ef9aad
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-03-08 18:57:05 +0800

    update mpi-sum and upload mpi-pi

--- MPI sum/MPI sum/mpi-sum.cpp
+++ MPI sum/MPI sum/mpi-sum.cpp
@@ -14,16 +14,23 @@ int main(int argc, int argv[])
 	double sum = 0;
 	double block = 0;
 
+	char p_name[MPI_MAX_PROCESSOR_NAME];
+	int p_namelen;
+
 	MPI_Init(0, 0);
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	MPI_Get_processor_name(p_name, &p_namelen);
+
+	cout << "rank " << my_rank << " on " << p_name << endl;
 
 	block = (argc != 1) ? argv[1] : MAX / comm_sz;
 	double startNum = (my_rank)*block + 1;
 	double endNum = (my_rank + 1) * block;
 
 	cout << setprecision(0);
-	cout << "my_rank=" << my_rank << setiosflags(ios::fixed) << " start=" << startNum << " end=" << endNum << endl;
+	//cout << "my_rank=" << my_rank << setiosflags(ios::fixed) << " start=" << startNum << " end=" << endNum << endl;
 	if (my_rank == 0) {
 		cout << setiosflags(ios::fixed) << "block size=" << block << endl;
 		for (int i = 1; i < comm_sz; i++) {
@@ -34,9 +41,12 @@ int main(int argc, int argv[])
 		}
 	}
 
-	for (; startNum <= endNum; startNum++) {
+	while (startNum <= endNum)
+	{
 		temp += startNum;
+		startNum++;
 	}
+
 	if (my_rank == 0)
 		sum += temp;
 	else
@@ -44,10 +54,10 @@ int main(int argc, int argv[])
 	temp = 0;
 
 	if (my_rank == 0) {
-		cout << "并行程序 " << comm_sz << "进程" << endl;
+		cout << "Comm_size " << comm_sz << endl;
 		cout << "sum=" << setiosflags(ios::fixed) << sum << endl;
 		cout << setprecision(3);
 	}
-	cout << "Process " << my_rank << " total time " << (double)clock() / CLK_TCK << "s." << endl;
+	cout << "Rank " << my_rank << " total time " << (double)clock() / CLK_TCK << "s." << endl;
 	MPI_Finalize();
 }
